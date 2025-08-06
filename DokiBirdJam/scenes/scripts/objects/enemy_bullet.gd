b5fd6535664ad9ca667aae52b5ca2cd1b4585a6d
parupo1467 <parupo1467@outlook.com>
2025-08-06 11:52:08 -0700

    Add Enemy Attack Projectiles

---
+++ DokiBirdJam/scenes/scripts/objects/enemy_bullet.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+var speed: int = 1000
+var destination: Vector2 = Vector2.ZERO
+var direction: Vector2 = Vector2.DOWN
+
+func _process(delta: float) -> void:
+	if position.distance_to(destination) <= 1:
+		queue_free()
+		
+	var velocity = position.direction_to(destination)
+	$Sprite2D.rotation = velocity.angle() + PI/2
+	position += velocity * speed * delta
