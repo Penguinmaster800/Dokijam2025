ab085c480a24b1cff0a9c9deec68f8844e275ff8
Penguinmaster800 <penguinmaster665@gmail.com>
2025-08-06 16:43:08 -0700

    ability progress
    small steps getting update from main

--- DokiBirdJam/scenes/scripts/objects/enemy_bullet.gd
+++ DokiBirdJam/scenes/scripts/objects/enemy_bullet.gd
@@ -5,7 +5,7 @@ var destination: Vector2 = Vector2.ZERO
 var direction: Vector2 = Vector2.DOWN
 
 func _process(delta: float) -> void:
-	if position.distance_to(destination) <= 1:
+	if position.distance_to(destination) <= 10:
 		queue_free()
 		
 	var velocity = position.direction_to(destination)
