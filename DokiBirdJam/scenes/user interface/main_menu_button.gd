a1891daf84ffdb1cc3d08ff644a289c413c83574
Penguinmaster800 <penguinmaster665@gmail.com>
2025-08-07 21:49:26 -0700

    Many scene location additions/changes
    Restructured scenes, added many scenes for the main levels. Working on transitions

--- DokiBirdJam/scenes/user interface/main_menu_button.gd
+++ DokiBirdJam/scenes/user interface/main_menu_button.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_button_pressed() -> void:
-	TransitionLayer.change_scene("res://scenes/levels/main_menu.tscn")
+	TransitionLayer.change_scene("res://scenes/menus/main_menu.tscn")
