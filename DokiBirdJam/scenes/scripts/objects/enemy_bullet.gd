3797b72281a426d26c636f16b7fa04c11b3326c4
parupo1467 <parupo1467@outlook.com>
2025-08-08 19:38:48 -0700

    Mod Enemy Bullet Rotation

--- DokiBirdJam/scenes/scripts/objects/enemy_bullet.gd
+++ DokiBirdJam/scenes/scripts/objects/enemy_bullet.gd
@@ -2,12 +2,14 @@ extends StaticBody2D
 
 var speed: int = 1500
 var destination: Vector2 = Vector2.ZERO
-var direction: Vector2 = Vector2.DOWN
+var velocity: Vector2
+
+func _ready() -> void:
+	velocity = position.direction_to(destination)
+	$Sprite2D.rotation = velocity.angle() + PI/2
 
 func _process(delta: float) -> void:
 	if position.distance_to(destination) <= 30:
 		queue_free()
 		
-	var velocity = position.direction_to(destination)
-	$Sprite2D.rotation = velocity.angle() + PI/2
 	position += velocity * speed * delta
