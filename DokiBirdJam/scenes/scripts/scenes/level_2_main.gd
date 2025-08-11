6348c8df15e95aad66947230b6cd01a003225aa2
parupo1467 <parupo1467@outlook.com>
2025-08-10 20:22:51 -0700

    Add Level Warehouse

--- DokiBirdJam/scenes/scripts/scenes/level_2_main.gd
+++ DokiBirdJam/scenes/scripts/scenes/level_2_main.gd
@@ -4,5 +4,15 @@ func _ready():
 	level_startup()
 	Status.enemies_remaining = 2
 	Status.time_remaining = 80
+	Status.doki_max_ammo = 6
+	Status.doki_max_health = 10
 	Status.doki_health = Status.doki_max_health
 	Status.doki_ammo = Status.doki_max_ammo
+	Status.level = 2
+	AudioManager.level_one_music.play()
+
+
+	spawn_enemy_gunman(EnumRowNo.RowNo.ROW1)
+	spawn_enemy_gunman(EnumRowNo.RowNo.ROW2)
+	spawn_enemy_gunman(EnumRowNo.RowNo.ROW3)
+	spawn_enemy_sniper(EnumRowNo.RowNo.ROW4)
