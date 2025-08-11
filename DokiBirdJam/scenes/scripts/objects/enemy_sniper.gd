a8d1887518d5961ba083426154c659ae1a9d1b4d
parupo1467 <parupo1467@outlook.com>
2025-08-11 13:41:40 -0700

    Fix Sniper Attack

--- DokiBirdJam/scenes/scripts/objects/enemy_sniper.gd
+++ DokiBirdJam/scenes/scripts/objects/enemy_sniper.gd
@@ -1,15 +1,31 @@
 extends EnemyParent
 class_name EnemySniper
 
-var is_aiming: bool = false
-
 signal aiming(pos: Vector2)
 signal stop_aiming
+
+func _process(delta: float) -> void:
+	super._process(delta)
+	
+	if current_stance == Stance.ATTACK:
+		if not Status.in_cover:
+			if $TimerCanShoot.is_stopped():
+				$TimerCanShoot.start()
+		else:
+			$TimerCanShoot.stop()
 
 func stance_to_attack():
 	super.stance_to_attack()
 	aiming.emit($BulletStartPosition/Marker2D.global_position)
 
 func stance_attack_cooldown():
+	$TimerCanShoot.stop()
 	super.stance_attack_cooldown()
 	stop_aiming.emit()
+
+func attack():
+	pass
+
+func _on_timer_can_shoot_timeout() -> void:
+	print("Sniper Shot!")
+	super.attack()
