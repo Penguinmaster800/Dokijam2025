87df7d2cc1e3662f5cdb8a02dbfcaa95f964d4c7
Penguinmaster800 <penguinmaster665@gmail.com>
2025-08-10 23:41:53 -0700

    Doki Firing sounds work
    Added dry fire, doki shoot, and reload

--- DokiBirdJam/globals/audio_manager.gd
+++ DokiBirdJam/globals/audio_manager.gd
@@ -2,3 +2,5 @@ extends Node
 @onready var level_one_music: AudioStreamPlayer2D = $LevelOneMusic
 @onready var doki_shoot: AudioStreamPlayer2D = $DokiShoot
 @onready var game_over_music: AudioStreamPlayer2D = $GameOverMusic
+@onready var doki_dry_fire: AudioStreamPlayer2D = $DokiDryFire
+@onready var doki_reload: AudioStreamPlayer2D = $DokiReload
