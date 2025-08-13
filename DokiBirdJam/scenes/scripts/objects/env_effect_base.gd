afdec21205e5be5b842cfea2fcb6c23d5731edb3
parupo1467 <parupo1467@outlook.com>
2025-08-12 20:40:15 -0700

    Add Enemy Wet Shock Effect Damage

--- DokiBirdJam/scenes/scripts/objects/env_effect_base.gd
+++ DokiBirdJam/scenes/scripts/objects/env_effect_base.gd
@@ -5,6 +5,7 @@ var row_no: EnumRowNo.RowNo
 
 func _on_area_entered(area: Area2D) -> void:
 	get_entered_enemy_node(area)
+	get_entered_object_node(area)
 
 func get_entered_enemy_node(area: Area2D) -> Node:
 	var enemy_node = area.get_parent()
@@ -20,3 +21,21 @@ func get_entered_enemy_node(area: Area2D) -> Node:
 		return
 
 	return enemy_node
+
+func get_entered_object_node(area: Area2D) -> Node:
+	if not (area is EnvObjectParent):
+		return
+
+	var row_node = area.get_parent().get_parent()
+	if not (row_node is RowParent):
+		return
+
+	var object_row_no = row_node.row_no
+	if row_no != object_row_no:
+		return
+
+	return area
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
