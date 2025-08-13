134f9f648066c3e7474808c3331824f4a2f82bd5
parupo1467 <parupo1467@outlook.com>
2025-08-13 12:08:05 -0700

    Add Enemy Waves

--- DokiBirdJam/scenes/scripts/scenes/level_2_main.gd
+++ DokiBirdJam/scenes/scripts/scenes/level_2_main.gd
@@ -2,7 +2,7 @@ extends LevelParent
 
 func _ready():
 	level_startup()
-	Status.enemies_remaining = 2
+	Status.enemies_remaining = 8
 	Status.time_remaining = 150
 	Status.doki_health = Status.doki_max_health
 	Status.doki_ammo = Status.doki_max_ammo
@@ -10,8 +10,25 @@ func _ready():
 	Status.doki_shot = 0
 	AudioManager.level_one_music.play()
 
+func spawn_wave(wave_number: int):
+	match wave_number:
+		1:
+			spawn_wave_1()
+		2:
+			spawn_wave_2()
 
-	spawn_enemy_gunman(EnumRowNo.RowNo.ROW1)
-	spawn_enemy_gunman(EnumRowNo.RowNo.ROW2)
-	spawn_enemy_gunman(EnumRowNo.RowNo.ROW3)
+func spawn_wave_1():
+	wave_enemy_count = 4
 	spawn_enemy_sniper(EnumRowNo.RowNo.ROW4)
+	spawn_enemy_brute_random_row()
+	spawn_enemy_gunman_random_row()
+	spawn_enemy_gunman_random_row()
+	spawn_env_object_drone(EnumRowNo.RowNo.ROW1)
+
+func spawn_wave_2():
+	wave_enemy_count = 4
+	spawn_enemy_sniper(EnumRowNo.RowNo.ROW4)
+	spawn_enemy_brute_random_row()
+	spawn_enemy_gunman_random_row()
+	spawn_enemy_gunman_random_row()
+	spawn_env_object_drone(EnumRowNo.RowNo.ROW1)
