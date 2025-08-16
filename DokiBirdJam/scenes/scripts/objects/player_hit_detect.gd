d92a77c9e94496259a1690af250a8dd5d4d5a02c
parupo1467 <parupo1467@outlook.com>
2025-08-15 21:18:42 -0700

    Add Player Hit Flash

--- DokiBirdJam/scenes/scripts/objects/player_hit_detect.gd
+++ DokiBirdJam/scenes/scripts/objects/player_hit_detect.gd
@@ -5,6 +5,7 @@ func _on_body_entered(body: Node):
 	if Abilities.going_ghost_active == false:
 		#add sound for getting hit
 		Status.doki_health -= damage
+		$"../AnimationPlayer".play("HitFlash")
 		print(Status.doki_health)
 		body.queue_free()
 	if Abilities.going_ghost_active == true:
