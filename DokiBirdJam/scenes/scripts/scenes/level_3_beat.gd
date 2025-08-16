9d0efbd43285af426c3fb1ded57d4110c6b50816
parupo1467 <parupo1467@outlook.com>
2025-08-16 10:09:05 -0700

    Add Upgrade Screen Image

--- DokiBirdJam/scenes/scripts/scenes/level_3_beat.gd
+++ DokiBirdJam/scenes/scripts/scenes/level_3_beat.gd
@@ -3,3 +3,6 @@ extends Node2D
 
 func _on_button_pressed() -> void:
 	TransitionLayer.change_scene("res://scenes/levels/level_1_main.tscn")
+
+func _on_main_menu_button_pressed() -> void:
+	TransitionLayer.change_scene("res://scenes/menus/main_menu.tscn")
